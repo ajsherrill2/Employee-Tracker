fc2cdafa6b856d022de8841fd837098d15b5b564
GH0ST <adamjamessherrill@gmail.com>
2022-10-22 14:34:20 -0500

    updated schema tables and created seeds

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -3,4 +3,33 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employees ();
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    manager_id INT DEFAULT NULL
+    PRIMARY KEY (id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
